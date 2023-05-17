4ed0a3944762be3dfcf746a6d8d1da07fcccfb74
Kennie <kennethchan.mk@gmail.com>
2023-05-17 19:17:54 -0400

    implement Typography

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
@@ -11,7 +12,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        Sidebar
+       < Sidebar />
+        <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
+          Copyright 2022 JSM Media
+        </Typography>
       </Box>
     </Stack>
   );
