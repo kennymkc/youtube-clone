1720d70ebcab44e55ce96443f2d8194f10cf9e45
Kennie <kennethchan.mk@gmail.com>
2023-05-19 13:42:03 -0400

    make sidebar buttons dynamic using state

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Sidebar, Videos } from "./";
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory]);
+  
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -13,7 +21,12 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-       < Sidebar />
+
+        < Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        
         <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
           Copyright 2022 JSM Media
         </Typography>
