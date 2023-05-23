03cbdc72faf1733301798ae5fe6258aa91ff7668
Kennie <kennethchan.mk@gmail.com>
2023-05-23 11:54:19 -0400

    add video title to card content

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -16,6 +16,13 @@ const VideoCard = ({ video: {id: {videoId} , snippet} }) => {
           sx={{width: 358, height: 180}}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }} >
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
