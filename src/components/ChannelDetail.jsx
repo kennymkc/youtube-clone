bd010fb33a8ca738a7a6e0d17f21c5b57c50f947
Kennie <kennethchan.mk@gmail.com>
2023-06-01 08:37:20 -0400

    fetch videos from channel

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -8,14 +8,18 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
   const { id } = useParams();
 
-  console.log(channelDetail)
+  console.log(channelDetail, videos)
 
   useEffect(() => {
-    fetchFromAPI(`channels?part="snippet&id=${id}`)
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
       .then((data) => setChannelDetail(data?.items[0]));
+    
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
   }, [id])
 
   return (
