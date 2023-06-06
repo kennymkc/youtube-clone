c86d5434a43639087c1e89d8ef6232d6fa4558cf
Kennie <kennethchan.mk@gmail.com>
2023-06-06 17:22:58 -0400

    fetch video using api call and display

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,12 +9,20 @@ import { Video } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet.statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]));
+  }, [id]);
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{xs: 'column', md: 'row'}}>
         <Box flex={1}>
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
-            <ReactPlayer />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
           </Box>
         </Box>
       </Stack>
