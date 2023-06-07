cb7d685f930ab3ee2257d4f763bc6c56e3be101f
Kennie <kennethchan.mk@gmail.com>
2023-06-07 17:19:38 -0400

    display video and channel title

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -17,12 +17,24 @@ const VideoDetail = () => {
       .then((data) => setVideoDetail(data.items[0]));
   }, [id]);
 
+  const { snippet: {title, channelId, channelTitle}, statistics: {viewCount, likeCount} } = videoDetail;
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{xs: 'column', md: 'row'}}>
         <Box flex={1}>
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{color: "#fff"}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography>
+                  {channelTitle}
+                </Typography>
+              </Link>
+            </Stack>
           </Box>
         </Box>
       </Stack>
