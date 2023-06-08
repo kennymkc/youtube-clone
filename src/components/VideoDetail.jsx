ac955f28fb41fc07e65da01775390a02d9ab28a4
Kennie <kennethchan.mk@gmail.com>
2023-06-08 12:16:31 -0400

    get api call for related videos

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,11 +10,15 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet.statistics&id=${id}`)
       .then((data) => setVideoDetail(data.items[0]));
+    
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items));
   }, [id]);
 
   if (!videoDetail?.snippet) return 'Loading...';
