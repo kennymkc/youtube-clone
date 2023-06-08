cfda5887550c213342d1b7925f0b96d226725107
Kennie <kennethchan.mk@gmail.com>
2023-06-08 12:33:48 -0400

    add conditional to make sure video is loaded

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
-const Videos = ({videos, direction}) => {
+const Videos = ({ videos, direction }) => {
+  
+  if (!videos?.length) return 'Loading...';
 
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
